21ab3f434dc0d478ed41d718e4df444351ccf391
Effy <88544295+s-b3rry@users.noreply.github.com>
2022-09-07 16:30:04 +0200

    casshier system

---
+++ casshier_system.py
@@ -0,0 +1,15 @@
+import locale
+locale.setlocale(locale.LC_ALL, 'de_DE')
+
+# input of price and payment
+price = input("Gib hier den Preis ein: ")
+payment = input("Der Kunde zahlt: ")
+
+# inputs will be turned into floats, no matter if they are written with . or ,
+price = locale.atof(price)
+payment = locale.atof(payment)
+
+change = payment - price
+
+# Output of the change, written with ,
+print("Das Rückgeld beträgt:", locale.format_string('%.2f', change), "€")
